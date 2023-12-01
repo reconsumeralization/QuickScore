a8f788f35862ef62a03a1afa11fb52219d49b683
reconsumeralization <reconsumeralization@gmail.com>
2023-12-01 05:30:59 -0500

    Update config.py

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,26 +1,21 @@
+User
 from typing import Optional
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    """
-    A class to manage all the settings for the application.
-    Uses pydantic BaseSettings for validation and serialization.
-    """
-    DB_HOST: Optional[str]
-    DB_PORT: Optional[str]
-    DB_USERNAME: Optional[str]
-    DB_PASSWORD: Optional[str]
-    DB_DATABASE: Optional[str]
+    DB_HOST: Optional[str] = None
+    DB_PORT: Optional[str] = None
+    DB_USERNAME: Optional[str] = None
+    DB_PASSWORD: Optional[str] = None
+    DB_DATABASE: Optional[str] = None
 
-    SECRET_KEY: Optional[str]
-    COHERE_API_KEY: Optional[str]
-    WEAVIATE_API_KEY: Optional[str]
-    WEAVIATE_URL: Optional[str]
+    SECRET_KEY: Optional[str] = None
+    COHERE_API_KEY: Optional[str] = None
+    WEAVIATE_API_KEY: Optional[str] = None
+    WEAVIATE_URL: Optional[str] = None
+    
+    model_config = SettingsConfigDict(env_file="./backend/.env")
 
-    class Config:
-        """
-        Internal class for configuring the behaviour of Settings.
-        """
-        env_file = "./backend/.env"
 
 config = Settings()
