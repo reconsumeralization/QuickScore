300b8fbe7701d320b73b3b7981ed59516f945f69
reconsumeralization <reconsumeralization@gmail.com>
2023-12-01 05:55:14 -0500

    Update config.py

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,9 +2,7 @@ from typing import Optional
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    """
-    Pydantic model for environment configuration.
-    """
+    
     DB_HOST: Optional[str] = None
     DB_PORT: Optional[int] = None
     DB_USERNAME: Optional[str] = None
@@ -17,10 +15,7 @@ class Settings(BaseSettings):
     WEAVIATE_URL: Optional[str] = None
 
     class Config:
-        """
-        Configuration for Pydantic model.
-        This class is used to specify the location of the environment file.
-        """
+        
         env_file = "./backend/.env"
 
 
