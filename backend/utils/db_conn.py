f0688ba1aadef52a210e4005cbd7ef0d5502f2b9
reconsumeralization <reconsumeralization@gmail.com>
2023-12-01 06:37:45 -0500

    Update db_conn.py

--- backend/utils/db_conn.py
+++ backend/utils/db_conn.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.ext.declarative import declarative_base
 
-from backend.config.config import Config
-from backend.utils.errors import DatabaseError
+from backend.config.config import config
+from backend.utils.errors import databaseError
 
 Base = declarative_base()
 
