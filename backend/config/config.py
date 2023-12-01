ce7a3768a9091c0524ace2d924030b3e8b104587
reconsumeralization <reconsumeralization@gmail.com>
2023-12-01 06:08:30 -0500

    Update config.py

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,18 +1,20 @@
 from typing import Optional
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     DB_HOST: Optional[str] = None
-    DB_PORT: Optional[int] = None
+    DB_PORT: Optional[str] = None
     DB_USERNAME: Optional[str] = None
     DB_PASSWORD: Optional[str] = None
     DB_DATABASE: Optional[str] = None
+
     SECRET_KEY: Optional[str] = None
     COHERE_API_KEY: Optional[str] = None
     WEAVIATE_API_KEY: Optional[str] = None
     WEAVIATE_URL: Optional[str] = None
+    
+    model_config = SettingsConfigDict(env_file="./backend/.env")
 
-    class Config:
-        env_file = "./backend/.env"
 
 config = Settings()
