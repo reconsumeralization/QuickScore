9bd6beb3eeb81b06231a285c7ef7dbc7e0941aa1
reconsumeralization <reconsumeralization@gmail.com>
2023-12-01 07:06:55 -0500

    Update students.py

--- frontend/students.py
+++ frontend/students.py
@@ -7,41 +7,40 @@ from backend.core.student_core import StudentCore
 st.session_state.setdefault('student_details', [])
 st.session_state.setdefault('show_overlay', False)
 
-def create_students():
-    populate_students_table()
-    render_side_bar()
+def render_student_form():
+    with st.form(key='student_details_form'):
+        name = st.text_input('Name', key='name')
+        email = st.text_input('Email', key='email')
+        roll_number = st.text_input('Roll Number', key='roll_number')
+        submitted = st.form_submit_button('Submit')
+        if submitted:
+            handle_student_submission(name, email, roll_number)
 
-    st.title("Students")
+def handle_student_submission(name, email, roll_number):
+    student_data = {
+        'name': name,
+        'email': email,
+        'roll_no': roll_number,
+        'user_id': st.session_state.user_id
+    }
 
-    # Overlay toggle
-    st.session_state.setdefault('show_overlay', False)
+    try:
+        student_core = StudentCore()
+        student_core.create_student(student_data)
+    except Exception as error:
+        st.error("Failed to add student")
+    st.experimental_rerun()
 
-    with st.expander("Upload Student Details"):
-        with st.container():
-            with st.form(key='student_details_form'):
-                name = st.text_input('Name', key='name')
-                email = st.text_input('Email', key='email')
-                roll_number = st.text_input('Roll Number', key='roll_number')
-                # Submit button for the form
-                submitted = st.form_submit_button('Submit')
-                if submitted:
-                    # Call the function to add a student
-                    add_student(name, email, roll_number)
-                    st.session_state.show_overlay = False
-
-    # Display the table of student details with 'View', 'Edit', and 'Delete' buttons
+def render_student_table():
     if st.session_state.student_details:
         st.markdown("<br>", unsafe_allow_html=True)
-        # Create a DataFrame for the table
         df = pd.DataFrame(st.session_state.student_details)
 
-        # Display column headers
         col_headers = st.columns((1, 1, 1, 1, 0.5, 0.5))
         headers = ["S.No", "Name", "Email", "Roll Number", "Edit", "Delete"]
         for col_header, header in zip(col_headers, headers):
             col_header.markdown(f'<h5 style="color: #4F8BF9;"><strong>{header}</strong></h5></div>', unsafe_allow_html=True)
 
-        # Iterate over the DataFrame to display the table with buttons
         for i, row in df.iterrows():
             student_id = row["student_id"]
             cols = st.columns((1, 1, 1, 1, 0.5, 0.5))
@@ -50,13 +49,23 @@ def create_students():
             cols[2].write(row['Email'])
             cols[3].write(row['Roll Number'])
 
-            # Action buttons
             if cols[4].button('✏️', key=f"edit_{i}"):
-                # Implement edit logic
-                pass
+                pass  # Implement edit logic
             if cols[5].button('🗑️', key=student_id):
                 delete_student(student_id)
                 st.experimental_rerun()
+
+def create_students():
+    populate_students_table()
+    render_side_bar()
+
+    st.title("Students")
+
+    with st.expander("Upload Student Details"):
+        with st.container():
+            render_student_form()
+
+    render_student_table()
 
 def populate_students_table():
     user_id = st.session_state.user_id
@@ -87,19 +96,4 @@ def delete_student(student_id):
         student_core.delete_student(student_id)
     except Exception as error:
         st.error("Cannot delete the student record!")
-    st.experimental_rerun()
-
-def add_student(name, email, roll_number):
-    student_data = {
-        'name': name,
-        'email': email,
-        'roll_no': roll_number,
-        'user_id': st.session_state.user_id
-    }
-    
-    try:
-        student_core = StudentCore()
-        student_core.create_student(student_data)
-    except Exception as error:
-        st.error("Failed to add student")
     st.experimental_rerun()
